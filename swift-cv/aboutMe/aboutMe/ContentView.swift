85143caa92c20f48958e3c9b2eec74e9cee380cf
Mafe Bracho <mafe.bracho@gmail.com>
2021-04-17 10:44:17 +0200

    start refractoring of repeating code

--- swift-cv/aboutMe/aboutMe/ContentView.swift
+++ swift-cv/aboutMe/aboutMe/ContentView.swift
@@ -35,56 +35,28 @@ struct ContentView: View {
                         .padding(.bottom)
                     VStack(alignment: .leading) {
                         HStack {
-                            let myNumber = "0162 276 7407"
-                            Button(action: {
-                                let phone = "tel:/"
-                                let formattedStr = phone + myNumber
-                                guard let url = URL(string: formattedStr) else { return }
-                                UIApplication.shared.open(url)
-                            }) {
-                                Text(myNumber)
-                                    .foregroundColor(.white)
-                                    .font(Font.custom("Roboto-Light", size: 20))
-                                    .padding(.leading)
-                                    .scaleEffect(hovered ? 2.0 : 1.0)
-                                    .animation(.default, value: hovered)
-                                    .onHover { isHovered in
-                                        self.hovered = isHovered
-                                    }
-                            }
-                            Spacer()
-                            Text("E-mail")
-                                .foregroundColor(.white)
-                                .font(Font.custom("Roboto-Light", size: 20))
-                                .scaleEffect(hovered ? 2.0 : 1.0)
-                                .animation(.default, value: hovered)
-                                .onHover { isHovered in
-                                    self.hovered = isHovered
+                            tappableText(withText: "Phone")
+                                .padding(.leading)
+                                .onTapGesture {
+                                    let myNumber = "Phone"
+                                    let phone = "tel:/"
+                                    let formattedStr = phone + myNumber
+                                    guard let url = URL(string: formattedStr) else { return }
+                                    UIApplication.shared.open(url)
                                 }
+                            
                             Spacer()
-                            Text("Linkedin")
-                                .foregroundColor(.white)
-                                .font(Font.custom("Roboto-Light", size: 20))
-                                .scaleEffect(hovered ? 2.0 : 1.0)
-                                .animation(.default, value: hovered)
-                                .onHover { isHovered in
-                                    self.hovered = isHovered
-                                }
+                            tappableText(withText: "E-mail")
+                            Spacer()
+                            tappableText(withText: "Linkedin")
                                 .onTapGesture {
                                     let url = URL.init(string: "https://www.linkedin.com/in/maria-fernanda-bracho/?locale=en_US")
                                     guard let linkedIn = url, UIApplication.shared.canOpenURL(linkedIn) else { return }
                                     UIApplication.shared.open(linkedIn)
                                 }
                             Spacer()
-                            Text("Github")
-                                .foregroundColor(.white)
-                                .font(Font.custom("Roboto-Light", size: 20))
+                            tappableText(withText: "Github")
                                 .padding(.trailing)
-                                .scaleEffect(hovered ? 2.0 : 1.0)
-                                .animation(.default, value: hovered)
-                                .onHover { isHovered in
-                                    self.hovered = isHovered
-                                }
                                 .onTapGesture {
                                     let url = URL.init(string: "https://github.com/mafebracho")
                                     guard let gitHub = url, UIApplication.shared.canOpenURL(gitHub) else { return }
@@ -109,6 +81,17 @@ struct ContentView: View {
             }
             
         }
+    }
+    
+    private func tappableText(withText text: String) -> some View {
+        Text(text)
+            .foregroundColor(.white)
+            .font(Font.custom("Roboto-Light", size: 20))
+            .scaleEffect(hovered ? 2.0 : 1.0)
+            .animation(.default, value: hovered)
+            .onHover { isHovered in
+                self.hovered = isHovered
+            }
     }
     
     struct ContentView_Previews: PreviewProvider {
